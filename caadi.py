a6885a1b5ba8a6e6a6a7fd8516cd2ec77376d91f
Mauricio Aguilar <mv.aguilargarcia@hotmail.com>
2023-06-13 13:06:33 -0500

    Added comments & fixed side_frame layout

--- caadi.py
+++ caadi.py
@@ -8,73 +8,108 @@ from tkinter import filedialog
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        # Initialize our data array
         self.data = []
 
+        # Establish our window dimensions
         self.title("CAADI")
         self.geometry("650x500")
 
+        # Create our main window panels
         self.main_panel = ttk.PanedWindow(orient="vertical")
         self.horiz_panel = ttk.PanedWindow(orient="horizontal")
 
+        # Define what our upper main_panel is composed of
+            #Frame that stores our components
         self.header_frame = tk.Frame(self.main_panel,bg="#1A1713",borderwidth=0)
-        self.image1_path = "escudo_ug.png"  
+            #Load the image
+        self.image1_path = "./img/escudo_ug.png"  
         self.image1 = Image.open(self.image1_path)
         self.image1 = self.image1.resize((70, 80))  
+            #Create a PhotoImage object from the image
         self.photo1 = ImageTk.PhotoImage(self.image1)
-        self.main_panel.add(self.header_frame)
+            # Create a Label widget to display the image
         self.image1_label = tk.Label(self.header_frame, image=self.photo1)
         self.image1_label.pack(side="top",padx=10)
-
+        
+        # Add our components to main_panel
+        self.main_panel.add(self.header_frame)
+        
+        # Define what our lower main_panel is composed of
+            #Frame that stores our components
         self.side_frame = tk.Frame(self.horiz_panel,bg="#DBD0C5")
-        self.bUpload = tk.Button(self.side_frame, text="Subir archivo", fg="white",bg="#C39A7A",border="0",padx="41",pady="10",command=self.open_file)
-        self.bUpload.pack(side="top")
-        self.lFilter = tk.Label(self.side_frame,text="Filtro: ", bg="#DBD0C5", pady=10,)
-        self.lFilter.pack()
+            #Button
+        self.bUpload = tk.Button(self.side_frame, text="Subir archivo", fg="white",bg="#C39A7A",border="0",height="2",command=self.open_file)
+        self.bUpload.pack(side="top", fill="x")
+            #Label
+        self.lFilter = tk.Label(self.side_frame,text="Filtro: ", bg="#DBD0C5")
+        self.lFilter.pack(fill="x", pady="15 0")
+            #Entry
         self.filterText = tk.StringVar()
         self.eFilter = tk.Entry(self.side_frame, textvariable=self.filterText)
-        self.eFilter.pack()
-        self.space = tk.Label(self.side_frame, bg="#DBD0C5",pady="5")
-        self.space.pack()
-        self.bFilter = tk.Button(self.side_frame,text="Aplicar filtro",fg="white",bg="#B8926A",border="0",padx="43",pady="10",command=self.apply_filter)
-        self.bFilter.pack()
-        self.bLimpiar = tk.Button(self.side_frame,text="Limpiar filtro",fg="white",bg="#854621",border="0",padx="41",pady="10",command=self.wipe_filter)
-        self.bLimpiar.pack()
-        self.bExit = tk.Button(self.side_frame, text="SALIR", fg="white", bg="#423025",border="0",padx="60",pady="10",command=exit)
-        self.bExit.pack(side="bottom")
+        self.eFilter.pack(fill="x", padx="5", pady="0 15")
+            #Button
+        self.bFilter = tk.Button(self.side_frame,text="Aplicar filtro",fg="white",bg="#B8926A",border="0",height="2",command=self.apply_filter)
+        self.bFilter.pack(fill="x")
+            #Button
+        self.bLimpiar = tk.Button(self.side_frame,text="Limpiar filtro",fg="white",bg="#854621",border="0",height="2",command=self.wipe_filter)
+        self.bLimpiar.pack(fill="x")
+            #Button
+        self.bExit = tk.Button(self.side_frame, text="SALIR", fg="white", bg="#423025",border="0",height="2",command=exit)
+        self.bExit.pack(side="bottom", fill="x")
+            #Add components to horiz_frame(LEFT SIDE)
         self.horiz_panel.add(self.side_frame)
 
+        #Define whate our horiz_frame(RIGHT SIDE) is gonna contain
+            #Frame
         self.main_frame = tk.Frame(self.horiz_panel)
+            #Canvas for scrolling
         self.main_canvas = tk.Canvas(self.main_frame)
+            #Table for our data
         self.table = tk.Frame(self.main_canvas, bg="#F2F3EC")
+            #Configure scrollbar
         self.scrollbar = tk.Scrollbar(self.main_canvas,orient="vertical",command=self.main_canvas.yview)
         self.main_canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.main_canvas.pack(fill="both", expand=True)
         self.scrollbar.pack(side="right",fill="y")
-
+        self.main_canvas.pack(fill="both", expand=True)
+        
+        #Bind "enter" with a function
         self.bind("<Return>", self.apply_filter)
 
+        #Add our RIGHT_SIDE to the lower panel
         self.horiz_panel.add(self.main_frame)
+        
+        #Add lower panel to the maine panel
+        self.main_panel.pack(fill="both",expand=True)
         self.main_panel.add(self.horiz_panel)
 
-        self.main_panel.pack(fill="both",expand=True)
-
+        
+    #Methods
     def apply_filter(self, event=None):
+        # Read the entry's text
         filter = self.filterText.get()
+        # Filter our data to obtain the matches
         filter_data = [self.title] + [row for row in self.rows if filter.lower() in str(row).lower()]
+        # Erase the table
         for widget in self.table.winfo_children():
             widget.destroy()
+        # Show only the filtered data
         for row, data_row in enumerate(filter_data):
             for col, dat in enumerate(data_row):
                 tk.Label(self.table, text=dat, relief=tk.GROOVE, width=20).grid(row=row, column=col)
 
     def wipe_filter(self, event=None):
+        # Delete the entry's text and reload the table
         self.eFilter.delete(0,"end")
         self.apply_filter()
 
     def show_table(self, event=None):
+        # Create and show the head of the table
         self.title = self.data[0]
         for col,name in enumerate(self.title):
             tk.Label(self.table, text=name, relief=tk.GROOVE, width=20).grid(row=0, column=col)
+        # Create and show the table
         self.rows = self.data[1:]
         for row,data_row in enumerate(self.rows): 
             for col, dat in enumerate(data_row):
@@ -85,6 +120,7 @@ class Window(tk.Tk):
         self.table.bind("<Configure>", lambda event, canvas=self.main_canvas: canvas.configure(scrollregion=canvas.bbox("all")))
     
     def open_file(self, event=None):
+        # Load the data and store it in datos
         file_path = filedialog.askopenfilename()
         with open(file_path, 'r') as archivo:
             lector_csv = csv.reader(archivo)
@@ -93,8 +129,11 @@ class Window(tk.Tk):
         self.show_table()
     
     def exit(self, event=None):
+            # Close the window
             self.destroy()     
 
 if __name__ == "__main__":
+    # Create a window class object
     window = Window()
+    # Execute the window
     window.mainloop()
